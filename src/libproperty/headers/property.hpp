3c77c0473fcdc1be022acb2fa180e28571a86df0
westelh <westelh@gmail.com>
2018-05-16 01:25:37 +0900

    wei

--- src/libproperty/headers/property.hpp
+++ src/libproperty/headers/property.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDED_ELH_PROPERTY_HPP
 #define INCLUDED_ELH_PROPERTY_HPP
 
+namespace elh {
+    template <class T>
+    class property final {
+        T value;
+    public:
+
+        template <class Args..>
+        property(Args.. args);
+    };
+}
+
 #endif
